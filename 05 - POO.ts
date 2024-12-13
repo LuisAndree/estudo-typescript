8b441a6ebf2d259b26b28824dbbd4504d4ace92a
Luís André <andre2016999@gmail.com>
2024-12-12 22:31:09 -0300

    POO heranca

--- 05 - POO.ts
+++ 05 - POO.ts
@@ -44,3 +44,22 @@ console.log(d1.cpf);
 d1.cpf = '611.490.743-56';
 console.log(d1.cpf);
 
+class Funcionario extends Dev {
+    private _salario: number;
+
+    constructor(nome: string, idade: number, altura: number, cpf: string, especialidade: string, senioridade: string, salario: number) {
+        super(nome, idade, altura, cpf, especialidade, senioridade);
+        this._salario = salario;
+    }
+
+    get salario(): number {
+        return this._salario;
+    }
+
+    set salario(newSalario) {
+        this._salario = newSalario;
+    }
+}
+
+const f1 = new Funcionario('Luis', 28, 1.80, '123.456.678-90', 'Vue.js', 'Estagiario', 1412);
+console.log(f1);
